1d8abeb314ecf4d1a00aea23e508d46d09bd44b3
Taras Kushnir <kushnirTV@gmail.com>
2016-10-18 09:24:31 +0300

    Added separate hash calculation

--- src/hasher.go
+++ src/hasher.go
@@ -5,22 +5,70 @@ import (
   "os"
   "io"
   "encoding/hex"
+  "path/filepath"
+  "sync"
 )
 
-func CalculateHash(filepath string) (<-chan string, <-chan error) {
-  resultChan := make(chan string)
-  errChan := make(chan error)
+type HashResult struct {
+  path string
+  hash string
+  err error
+}
+
+func CalculateHashes(root string) (map[string]string, err) {
+  c, errc := calculateSha1Hashes(root)
+
+  m := make(map[string]string)
+  for r := range c {
+    if r.err != nil {
+      return nil, r.err
+    }
+
+    key := filepath.Rel(root, r.path)
+    m[key] = r.hash
+  }
+
+  if err := <- errc; err != nil {
+    return nil, err
+  }
+
+  return m, nil
+}
+
+func calculateSha1Hashes(root string) (<-chan HashResult, <-chan error) {
+  c := make(chan HashResult)
+  errc := make(chan error, 1)
 
   go func() {
-    filehash, err := calculateFileHash(filepath)
-    if err != nil {
-      errChan <- err
-    } else {
-      resultChan <- filehash
-    }
+    var wg sync.WaitGroup
+    err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+      if err != nil {
+        return err
+      }
+
+      if !info.Mode().IsRegular() {
+        return nil
+      }
+
+      wg.Add(1)
+
+      go func() {
+        hash, err := calculateFileHash(path)
+        c <- HashResult{path, hash, err}
+
+        wg.Done()
+      }()
+    })
+
+    go func() {
+      wg.Wait()
+      close(c)
+    }()
+
+    errc <- err
   }()
 
-  return resultChan, errChan
+  return c, errc
 }
 
 func calculateFileHash(filepath string) (string, error) {
